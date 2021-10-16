2827bbf55950939b58a0f5ad47f271760dd8644f
Aditi542 <89867911+Aditi542@users.noreply.github.com>
2021-10-17 00:25:51 +0530

    Add files via upload

---
+++ Area of triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int b,h;
+    cout<<"Enter the value of base and height :";
+    cin>>b>>h;
+    cout<<"Area of triangle="<<(0.5*b*h);
+
+   return 0;
+}
