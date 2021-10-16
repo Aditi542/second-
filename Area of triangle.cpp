f77f2e1027ab48008e52103b8af579721e924b7c
Poorvanshi-sahu <poorvanshisahu837@gmail.com>
2021-10-17 00:44:56 +0530

    Typo changed

--- Area of triangle.cpp
+++ Area of triangle.cpp
@@ -1,11 +1,11 @@
-#include<iostream>
+#include<iostream> // header file
 using namespace std;
 int main()
 {
-    int b,h;
+    int b,h; //declaration
     cout<<"Enter the value of base and height :";
     cin>>b>>h;
-    cout<<"Area of triangle="<<(0.5*b*h);
+    cout<<"Area of triangle="<<(0.5*b*h); // final product
 
    return 0;
 }
